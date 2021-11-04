c015af85c429b274b457f0b88319baa3ac994645
CyberFlame <cyberflameu@gmail.com>
2021-11-05 12:19:09 +1300

    more of a start

--- src/main/java/net/cyberflame/lumberharvester/listeners/PlayerJoinListener.java
+++ src/main/java/net/cyberflame/lumberharvester/listeners/PlayerJoinListener.java
@@ -1,6 +1,7 @@
 package net.cyberflame.lumberharvester.listeners;
 
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,13 +9,16 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class PlayerJoinListener implements Listener {
+    @SuppressWarnings("deprecation")
     @EventHandler
     public void onBlockBreak(final BlockBreakEvent event) {
         Player player = event.getPlayer();
-        Material material = event.getBlock().getType();
-        byte blockData = event.getBlock().getData();
+        Block block = event.getBlock();
+        Material material = block.getType();
+        byte blockData = block.getData();
         if(material == Material.LOG || material == Material.LOG_2) {
             player.getInventory().addItem(new ItemStack(material, 1, blockData));
+            block.setType(Material.STONE);
 
         }
     }
