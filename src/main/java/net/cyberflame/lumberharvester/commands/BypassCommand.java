3ddf16d57ac63b566d73453a17028720fb72e52b
Aaron Lowe <cyberflameu@gmail.com>
2021-11-18 14:02:28 +1300

    refactoring and turned things to 'final'

--- src/main/java/net/cyberflame/lumberharvester/commands/BypassCommand.java
+++ src/main/java/net/cyberflame/lumberharvester/commands/BypassCommand.java
@@ -8,13 +8,11 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import net.md_5.bungee.api.ChatColor;
-
 public class BypassCommand implements CommandExecutor {
 
     private final Main instance = Main.getInstance();
-    private String permission = "lh.bypass";
-    private String noPermission = FormatUtils.NO_PERMISSION;
+    private final String permission = "lh.bypass";
+    private final String noPermission = FormatUtils.NO_PERMISSION;
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -25,22 +23,20 @@ public class BypassCommand implements CommandExecutor {
         if (args.length == 1) {
             if (Bukkit.getPlayer(args[0]) != null) {
                 Player target = Bukkit.getPlayer(args[0]);
-                String targetname = Bukkit.getPlayer(args[0]).getName();
+                String targetName = Bukkit.getPlayer(args[0]).getName();
                 instance.setBypassing(Bukkit.getPlayer(args[0]).getUniqueId());
                 FormatUtils.sendMessage(sender, (instance.getBypassing(target.getUniqueId()) ?
                                                          FormatUtils.BYPASS_OTHER_TOGGLE_ON.replaceAll("%player%",
-                                                                                                    targetname) :
+                                                                                                    targetName) :
                                                          FormatUtils.BYPASS_OTHER_TOGGLE_OFF.replaceAll("%player%",
-                                                                                                     targetname)));
+                                                                                                     targetName)));
                 FormatUtils.sendMessage(target, (instance.getBypassing(target.getUniqueId()) ?
                                                          FormatUtils.BYPASS_TOGGLE_ON.replaceAll("%player%",
-                                                                                               targetname) :
+                                                                                               targetName) :
                                                          FormatUtils.BYPASS_TOGGLE_OFF.replaceAll("%player%",
-                                                                                               targetname)));
+                                                                                               targetName)));
             }
-            else {
-                FormatUtils.sendMessage(sender, FormatUtils.UNKNOWN_PLAYER.replaceAll("%player%", args[0]));
-            }
+            else FormatUtils.sendMessage(sender, FormatUtils.UNKNOWN_PLAYER.replaceAll("%player%", args[0]));
             return true;
         }
         Player player = (Player) sender;
